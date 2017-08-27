4f202cfb2818f823ac517b8f881b1a438fee4854
jordierill <31383940+jordierill@users.noreply.github.com>
2017-08-27 13:57:21 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,37 +1,33 @@
-package sortingBalls;
+package spellItOut;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Spell it out main class
+ * @author jerill
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
 
-		Rack rack = new Rack();
-		
 		while (true) {
-	       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        System.out.print("Enter a new ball : ");
-	        
-	        try{
-	            int i = Integer.parseInt(br.readLine());
-	            
-	            rack.add(i);
-	            
-	            QuickSort.qSort(rack.balls, 0, rack.balls.length-1);
-	            
-	            for (int ball : rack.balls) {
-					System.out.println(ball);
-				}
-	            
-	        } catch (NumberFormatException nfe) {
-	            System.err.println("Invalid Format!");
-	        } catch (IOException e) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Enter a number to spell it out :");
+
+			try {
+				long i = Long.parseLong(br.readLine());
+
+				System.out.println( SpellItOut.convert(i));
+
+			} catch (NumberFormatException nfe) {
+				System.err.println("Invalid Format!");
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
-	}
 
+	}
 }
