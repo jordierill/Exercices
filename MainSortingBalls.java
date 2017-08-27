01c73602fe33fc977a1d974b735cf3e82b3751c7
jordierill <31383940+jordierill@users.noreply.github.com>
2017-08-27 17:32:30 +0200

    Add files via upload

---
+++ MainSortingBalls.java
@@ -0,0 +1,42 @@
+package sortingBalls;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Sorting balls main class
+ * @author jerill
+ *
+ */
+public class MainSortingBalls {
+
+	public static void main(String[] args) {
+
+		Rack rack = new Rack();
+
+		while (true) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("Enter a new ball : ");
+
+			try {
+				int i = Integer.parseInt(br.readLine());
+
+				rack.add(i);
+
+				QuickSort.qSort(rack.balls, 0, rack.balls.length - 1);
+
+				for (int ball : rack.balls) {
+					System.out.println(ball);
+				}
+
+			} catch (NumberFormatException nfe) {
+				System.err.println("Invalid Format!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ MainSpellItOut.java
@@ -0,0 +1,33 @@
+package spellItOut;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Spell it out main class
+ * @author jerill
+ *
+ */
+public class MainSpellItOut {
+
+	public static void main(String[] args) {
+
+		while (true) {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Enter a number to spell it out :");
+
+			try {
+				long i = Long.parseLong(br.readLine());
+
+				System.out.println( SpellItOut.convert(i));
+
+			} catch (NumberFormatException nfe) {
+				System.err.println("Invalid Format!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
